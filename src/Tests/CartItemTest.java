f1f01e58e4b7d5c34372568d6254f2b89813ad8e
Safet <sftaliev@gmail.com>
2022-04-05 13:05:16 +0300

    Test updates

--- src/Tests/CartItemTest.java
+++ src/Tests/CartItemTest.java
@@ -8,9 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class CartItemTest {
 
     @Test
-    void calculateTotalPrice() {
+    public void calculatedTotalPriceShouldBeHundredAndSixty() {
+        //given
         Product product = new Product("Star Wars", 80);
         CartItem item = new CartItem(product, 2);
-        assertEquals(160, item.calculateTotalPrice());
+
+        //when
+        double calculatedPrice = item.calculateTotalPrice();
+
+        //then
+        assertEquals(160, calculatedPrice);
     }
+
 }
--- src/Tests/CartTest.java
+++ src/Tests/CartTest.java
@@ -6,51 +6,79 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CartTest {
-
     @Test
-    public void cartPriceCalculatedCorrectly(){
-        Product product = new Product("GTA 5", 59.99);
-        CartItem item = new CartItem(product);
+    public void itemsInCartPriceShouldBeEighty(){
+        //given
+        Product product = new Product("Fifa 22", 40);
+        CartItem item = new CartItem(product, 2);
         Cart gta5 = new Cart();
         gta5.addProduct(item);
-        Assertions.assertEquals(59.99, gta5.calculateItemsPrice());
+
+        //when
+        double productPrice = gta5.calculateItemsPrice();
+
+        //then
+        Assertions.assertEquals(80, productPrice);
     }
 
     @Test
-    public void cartPriceShouldBe(){
+    public void vatPriceShouldBeTwelve(){
+        //given
         Product product = new Product("GTA 5", 60);
         CartItem item = new CartItem(product);
         Cart gta5 = new Cart();
         gta5.addProduct(item);
-        //20% of 60
-        Assertions.assertEquals(12, gta5.calculateVat());
+
+        //when
+        double vat = gta5.calculateVat();
+
+        //then
+        Assertions.assertEquals(12, vat);
     }
 
     @Test
     public void deliveryFeeShouldBeTen(){
+        //given
         Product product = new Product("GTA 5", 60);
         CartItem item = new CartItem(product);
         Cart gta5 = new Cart();
         gta5.addProduct(item);
-        Assertions.assertEquals(10, gta5.calculateDeliveryFee());
+
+        //when
+        double deliveryFee = gta5.calculateDeliveryFee();
+
+        //then
+        Assertions.assertEquals(10, deliveryFee);
     }
 
     @Test
     public void deliveryFeeShouldBeFive(){
-        Product product = new Product("GTA 5", 120);
+        //given
+        Product product = new Product("Lego Star Wars", 120);
         CartItem item = new CartItem(product);
         Cart gta5 = new Cart();
         gta5.addProduct(item);
-        Assertions.assertEquals(5, gta5.calculateDeliveryFee());
+
+        //when
+        double deliveryFee = gta5.calculateDeliveryFee();
+
+        //then
+        Assertions.assertEquals(5, deliveryFee);
     }
 
     @Test
     public void deliveryFeeShouldBeZero(){
+        //given
         Product product = new Product("GTA 5", 120);
         CartItem item = new CartItem(product, 2);
         Cart gta5 = new Cart();
         gta5.addProduct(item);
-        Assertions.assertEquals(0, gta5.calculateDeliveryFee());
+
+        //when
+        double deliveryFee = gta5.calculateDeliveryFee();
+
+        //then
+        Assertions.assertEquals(0, deliveryFee);
     }
 
 
