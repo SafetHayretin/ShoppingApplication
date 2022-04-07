11eb2d3bfb92a7e99579503aee2a5f351026e79f
Safet <sftaliev@gmail.com>
2022-04-07 12:59:53 +0300

    removed main

--- src/Application/MainApp.java
+++ src/Application/MainApp.java
@@ -5,9 +5,5 @@ import Application.CartItem;
 
 public class MainApp {
     public static void main(String[] args) {
-
-        Product gta = new Product("Fifa 22", 59.99);
-        Product fifa = new Product("Fifa 22", 59.99);
-        System.out.println(gta.equals(fifa));
     }
 }
